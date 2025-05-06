e976fa66874991adf6eb5c45ad5776bc649dc7d0
@akinyemi07 <soldinixpresini@gmail.com>
2025-05-06 14:21:56 +0100

    my first official javascript program

---
+++ even.js
@@ -0,0 +1,11 @@
+alert("a program to print if a number is even or odd ")
+
+//recieve input fron the user
+
+let number=parseInt(prompt("kindly enter the number; "));
+if (number % 2 ===0){
+	alert(`${number} is even`);
+}
+else{
+	alert(`${number} is odd`);
+	
