37658cb3f576e5e7a7c180211cd75e594370f56f
@akinyemi07 <soldinixpresini@gmail.com>
2025-05-13 13:12:11 +0100

    another commit in learning javascript

---
+++ bmi.js
@@ -0,0 +1,15 @@
+//a program to calculate the bmi of a person 
+
+//a would be used to represent the weight for ease 
+
+//b would be used to represent the height for ease 
+
+let a = parseFloat(prompt("kindly input the weight of the individual; "));
+
+let b = parseFloat(prompt("kindly input the height of the individual; "));
+
+bmi=(a/(b**2));
+
+// if (bmi  )
+
+alert(`the bmi is ${bmi}, i would get back to you on the bmi indicator shortly `)
